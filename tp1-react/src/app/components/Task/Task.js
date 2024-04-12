df8d39b6ce741e3d6067d0d27c37854da172c007
Alan Cristian Gaston Vera <93807967+veraAlan@users.noreply.github.com>
2024-04-11 21:09:13 -0300

    Added task

---
+++ tp1-react/src/app/components/Task/Task.js
@@ -0,0 +1,9 @@
+const Task = () => {
+   const data = "ALSKDJFALKSJDF";
+
+   return (
+      <p>This is a task, {data}</p>
+   );
+}
+
+export default Task;
--- tp1-react/src/app/pages/Home/Home.js
+++ tp1-react/src/app/pages/Home/Home.js
@@ -1,9 +1,27 @@
+import Task from "../../components/Task/Task.js";
+
+function loadTasks(listOfTasks) {
+   var elemHTML = "";
+   listOfTasks.forEach(element => {
+      elemHTML += element;
+   });
+
+   return elemHTML;
+}
+
 const Home = () => {
+   const tasks = ["data1", "data2", "data3"];
+
+   const elems = loadTasks(tasks);
+
    return (
       <main>
          <div>
             This page is the home
          </div>
+         <ul>
+            <li>{elems}</li>
+         </ul>
       </main>
    );
 }
