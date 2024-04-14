79ce5cc9184214b709031fa17e9059eae83ce507
Alan Cristian Gaston Vera <93807967+veraAlan@users.noreply.github.com>
2024-04-13 23:15:06 -0300

    Completed test
    Test working completely, style needs to be checked and possibly some code refactor/modularized.

--- tp1-react/src/app/pages/Home/Home.js
+++ tp1-react/src/app/pages/Home/Home.js
@@ -9,7 +9,7 @@ import style from "./Home.module.css";
 const data1 = { id: 0, info: "data n1", completed: false };
 const data2 = { id: 1, info: "data n2", completed: false };
 const data3 = { id: 2, info: "data n3", completed: false };
-const data4 = { id: 3, info: "data n3", completed: true };
+const data4 = { id: 3, info: "data n4", completed: true };
 const preloadTaskList = [data1, data2, data3, data4];
 
 // Set initial id at the last item of array.
@@ -18,13 +18,25 @@ let preloadId = Number([preloadTaskList.length - 1]) + 1;;
 
 const Home = () => {
    const [inputValue, setInputValue] = useState("");
-   const [taskList, setTaskState] = useState(preloadTaskList);
+   const [taskList, setTaskState] = useState(
+      preloadTaskList.filter((data) => data.completed === false)
+   );
+   const [doneTaskList, setDoneTaskState] = useState(
+      preloadTaskList.filter((data) => data.completed === true)
+   );
+   console.log("Updated task list", doneTaskList);
+
    const [nextId, setNextId] = useState(preloadId);
 
-   const updateTask = (index) => {
-      const updatedTask = [...taskList];
-      (updatedTask[index]).completed = true;
-      setTaskState(updatedTask);
+   // Debug this function.
+   const updateTask = (id) => {
+      const index = taskList.findIndex(task => task.id === id);
+      let updatedTask = taskList[index];
+      updatedTask.completed = true;
+      let updatedTaskList = taskList;
+      updatedTaskList.splice(index, 1);
+      setTaskState(updatedTaskList);
+      setDoneTaskState([...doneTaskList, updatedTask]);
    };
 
    const onChangeHandler = (event) => { setInputValue(event.target.value); };
@@ -54,16 +66,24 @@ const Home = () => {
             </div>
             <div className={style.containerItem}>
                <ul className={style.list} >
-                  {taskList.map((data) => {
-                     return <li className={style.listItem} key={data.id}>{Task(data.info, data.completed)}
-                        <Button className={style.text} text="Complete" onClickFn={() => updateTask(data.id)} />
-                     </li>;
-                  })}
+                  {
+                     // Create as a callback function for each render.
+                     taskList.map((data) => {
+                        return <li className={style.listItem} key={data.id}>{Task(data.info, data.completed)}
+                           <Button className={style.text} text="Complete" onClickFn={() => updateTask(data.id)} />
+                        </li>;
+                     })
+                  }
                </ul>
             </div>
             <div className={style.containerItem}>
                <ul className={style.list}>
-
+                  {
+                     // Callback function for each updated task.
+                     doneTaskList.map((data) => {
+                        return <li className={style.listItem} key={data.id}>{Task(data.info, data.completed)}</li>;
+                     })
+                  }
                </ul>
             </div>
          </div>
