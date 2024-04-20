07ede67203fd14729177429122d5884862826986
Alan Cristian Gaston Vera <93807967+veraAlan@users.noreply.github.com>
2024-04-20 20:51:30 -0300

    TODO
    Added small TODO.

--- tp1-react/src/app/components/Search/Search.jsx
+++ tp1-react/src/app/components/Search/Search.jsx
@@ -4,6 +4,8 @@ import Input from "../Input/Input";
 import Task from "../Task/Task";
 import style from "./Search.module.css";
 
+// Correct this so it can search the actual element of the task and the buttos can be used.
+// Maybe create as a callback from TasskList. So it can replace the actual TaskList by the Queried list. 
 export default function Search({ completed }) {
    const tasks = useContext(TaskContext).filter((task) => task.completed === completed);
    const [resultSearch, setResultSearch] = useState([]);
